07035dc90ba7666f253f4147f74b11c06b111bb2
LocTM <tranminhloc.anhk@gmail.com>
2023-11-27 15:04:31 +0700

    BT2

---
+++ index.js
@@ -0,0 +1,24 @@
+// Nhập vào hai số a và b, và kiểm tra xem a có chia hết cho b hay không
+// let a = prompt("Nhap vao so a: ");
+// let b = prompt("Nhap vao so b: ");
+// function cal(a, b){
+//     if (a % b === 0){
+//         console.log("a chia het cho b")
+//     }
+//     else{
+//         console.log("a khong chia het cho b")
+//     }
+// }
+// cal(a,b);
+
+// Nhập tuổi và in ra kết quả nếu tuổi học sinh đó không đủ điều kiện vào học lớp 10.
+// let a = prompt("Nhap vao so tuoi: ");
+// function checkTuoi(a){
+//     if (a < 16){
+//         console.log("Ban khong du tuoi")
+//     }
+//     else{
+//         console.log("Du tuoi")
+//     }
+// }
+// checkTuoi(a)
